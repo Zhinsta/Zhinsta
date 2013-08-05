c9b99fa54582ad35a5f2d85719385a4d76ba803c
isnowfy <isnowfy@gmail.com>
2013-08-05 20:56:38 +0800

    fix

--- 1/zhinsta/filters.py
+++ 1/zhinsta/filters.py
@@ -24,32 +24,29 @@ def time_since(dt, default='刚刚'):
     """将 datetime 替换成字符串 ('3小时前', '2天前' 等等)
     的 Jinja filter copy from
     https://github.com/tonyblundell/socialdump/blob/master/socialdump.py
-    sqlite 的 CURRENT_TIMESTAMP 只能使用 UTC 时间, 所以单元测试
-    看到时间是8小时前的 don't panic, PostgreSQL 是有时区设定的.
-
     """
-    # added by jade
     if not dt:
         return ''
 
     now = datetime.now()
     diff = now - dt
     diff = diff - timedelta(seconds=28800)
-    total_seconds = diff.total_seconds()
+    total_seconds = int(diff.total_seconds())
     if total_seconds > 0:
-        if total_seconds < 10800:  # 3 小时内
+        if total_seconds < 604800:
             periods = (
-                (diff.seconds / 3600, '小时'),
-                (diff.seconds / 60, '分钟'),
-                (diff.seconds, '秒'),
+                (total_seconds / 86400, '天'),
+                (total_seconds / 3600, '小时'),
+                (total_seconds / 60, '分钟'),
+                (total_seconds, '秒'),
             )
             for period, unit in periods:
                 if period > 0:
                     return '%d%s前' % (period, unit)
             return default
-        elif total_seconds < 86400 and dt.day == now.day:  # 严格的今天内
+        elif total_seconds < 86400 and dt.day == now.day:
             return '今天 ' + dt.strftime('%H:%M')
         else:
             return unicode(dt.strftime('%Y-%m-%d %H:%M'))
     else:
-        return default  # 别手贱写未来了...
+        return default
