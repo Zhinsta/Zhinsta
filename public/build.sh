1868431b04c1ff0424bffb8b82976b00a1f21ad3
zmmbreeze <zmmbreeze0825@gmail.com>
2013-08-18 23:48:14 +0800

    modify like action

--- public/build.sh
+++ public/build.sh
@@ -4,7 +4,7 @@ echo "START"
 
 for file in ./css/*.less
 do
-    f=${file:(6)}
+    f=${file:6}
     printf "lessc -c"
     printf " "
     printf $f
@@ -13,6 +13,23 @@ do
     printf "\n"
     f=${f/%\.less/\.css}
     lessc -c $file > "./css/${f}"
+done
+
+for file in ./js/*.js
+do
+    a=".min.js"
+    if [[ "${file/$a/}" != "$file" ]]
+    then
+        continue
+    fi
+    f=${file:5}
+    printf "uglifyjs "
+    printf $f
+    printf " "
+    printf ${f/%\.js/\.min.js}
+    printf "\n"
+    f=${f/%\.js/\.min\.js}
+    uglifyjs $file > "./js/${f}"
 done
 
 cp css/*.css ../../svn-zhinsta/1/zhinsta/static/css/
