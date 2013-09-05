46de201a49a629573fa2be418eaa8f5db7158230
isnowfy <isnowfy@gmail.com>
2013-09-05 14:48:10 +0800

    change session lifetime

--- 1/zhinsta/app.py
+++ 1/zhinsta/app.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
+
 from flask import Flask
+from flask import session
 
 from .settings import SECRET_KEY
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
+session.permanent = True
+app.permanent_session_lifetime = timedelta(days=30)
