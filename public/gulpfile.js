f8cf681fb9939bdff2cf479dc7779328c56f21c6
zmmbreeze <zmmbreeze0825@gmail.com>
2014-06-21 18:45:52 +0800

    Added gulp

---
+++ public/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var path = require('path');
+
+gulp.task('default', function() {
+    gulp.src('./css/**/g.less')
+        .pipe(less({
+          paths: [ path.join(__dirname, 'css') ]
+        }))
+        .pipe(gulp.dest('../zhinsta/static/css'));
+
+    gulp.src('./js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('../zhinsta/static/js'));
+});
